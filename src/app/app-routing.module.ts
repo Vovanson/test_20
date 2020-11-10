ebfe512de849d2b8d051fcc769fce675788a1909
vovan <shvvs@ukr.net>
2020-11-10 04:36:49 +0200

    1comit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,22 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { LogInComponent } from './log-in/log-in.component';
+import { ToDoListComponent } from './to-do-list/to-do-list.component';
+import { ToDoStatisticComponent } from './to-do-statistic/to-do-statistic.component';
+import { ToDoDetailComponent } from './to-do-detail/to-do-detail.component';
 
-const routes: Routes = [];
+import { AppComponent } from './app.component';
+
+const routes: Routes = [
+  { path: 'dashboard', component: DashboardComponent },
+  { path: 'log-in', component: LogInComponent },
+  { path: 'to-do-list', component: ToDoListComponent },
+  { path: 'to-do-statistic', component: ToDoStatisticComponent },
+  { path: 'to-do-detail/:id', component: ToDoDetailComponent },
+  { path: 'app', component: AppComponent },
+  { path: '', redirectTo: '/log-in', pathMatch: 'full' },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,41 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms'; 
+import { HttpClientModule } from '@angular/common/http';
+
+import { HttpClientInMemoryWebApiModule } from 'angular-in-memory-web-api';
+import { InMemoryDataService } from '../servise/in-memory-data.service';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { LogInComponent } from './log-in/log-in.component';
+import { ToDoListComponent } from './to-do-list/to-do-list.component';
+import { ToDoStatisticComponent } from './to-do-statistic/to-do-statistic.component';
+import { ToDoDetailComponent } from './to-do-detail/to-do-detail.component';
+import { MessagesComponent } from './messages/messages.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DashboardComponent,
+    LogInComponent,
+    ToDoListComponent,
+    ToDoStatisticComponent,
+    ToDoDetailComponent,
+    MessagesComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule,
+      // The HttpClientInMemoryWebApiModule module intercepts HTTP requests
+    // and returns simulated server responses.
+    // Remove it when a real server is ready to receive requests.
+    HttpClientInMemoryWebApiModule.forRoot(
+    InMemoryDataService, { dataEncapsulation: false }
+)
   ],
   providers: [],
   bootstrap: [AppComponent]
